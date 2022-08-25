5e52ebbf7607d6e9537256b9f8df3e0b6bf7a98a
Seth David Gyimah <esec26@gmail.com>
2022-08-25 01:11:37 +0000

    self count sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false    
   end
 
   def question?
-
+    self.end_with?("?") ? true : false    
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false    
   end
 
+  #define endpoints (exclamation, full stop, question mark)
+  #split self or string based on endpoint, filter array, return number of elements not empty. 
   def count_sentences
-
+    delimeters = ['!', '.', '?']
+    self.split(Regexp.union(delimeters)).select {|item| !item.empty?}.size
   end
 end
